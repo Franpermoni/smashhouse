483acefebe1283a82d28715a73f6c1503cccb5d3
Francisco Permoni <permonifran@gmail.com>
2026-03-29 18:44:33 -0300

    Aparece un panel cuando tocas el item de carrito

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// ================================
-// PASO 1 Y 2: ABRIR Y CERRAR EL PANEL
-// ================================
+// ABRIR Y CERRAR EL PANEL
+const carritoPanel = document.querySelector('#carrito-panel')
+const cerrarCarrito = document.querySelector('#cerrar-carrito')
+const carritoIcono = document.querySelector('.cart-icon-wrap')
 
-// Agarramos los elementos del HTML que necesitamos
+// Abrir panel al clickear el 🛒
+carritoIcono.addEventListener('click', function() {
+    carritoPanel.classList.add('abierto')
+})
+
+// Cerrar panel al clickear la ✕
+cerrarCarrito.addEventListener('click', function() {
+    carritoPanel.classList.remove('abierto')
+})
