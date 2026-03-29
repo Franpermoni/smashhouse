ea3bf822b0a5c3b6501e3d64acdcef57912e7c65
Francisco Permoni <permonifran@gmail.com>
2026-03-29 18:36:39 -0300

    Agregando el carrito de vuelta

--- script.js
+++ script.js
@@ -3,16 +3,3 @@
 // ================================
 
 // Agarramos los elementos del HTML que necesitamos
-const carritoPanel = document.querySelector('#carrito-panel')
-const cerrarCarrito = document.querySelector('#cerrar-carrito')
-const carritoIcono = document.querySelector('.cart-icon-wrap')
-
-// Cuando hacen click en el ícono 🛒 → abre el panel
-carritoIcono.addEventListener('click', function() {
-    carritoPanel.classList.add('abierto')  // le agrega la clase "abierto"
-})
-
-// Cuando hacen click en la ✕ → cierra el panel
-cerrarCarrito.addEventListener('click', function() {
-    carritoPanel.classList.remove('abierto')  // le saca la clase "abierto"
-})
